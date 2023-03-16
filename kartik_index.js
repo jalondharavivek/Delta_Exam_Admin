5069f787743fe083d1df4ce7beabf93bdb4a508d
mk7102 <manoj.bajiya.2023@gmail.com>
2023-03-16 18:38:31 +0530

    23DEV060: Add category in index file

--- kartik_index.js
+++ kartik_index.js
@@ -1,32 +1,28 @@
-const express = require('express')
-const path = require('path')
-const app = express();
-const mysql = require("mysql2/promise");
-const port = 8765;
+ const express = require('express')
+ const path = require('path')
+ const app = express();
+ const mysql = require("mysql2/promise");
+ const port = 8765;
+ app.set("view engine", "ejs");
 
-app.set("view engine", "ejs");
+ app.use(express.static('public'));
+ app.use(express.static(path.join(__dirname, '/public')))
+ const db = mysql.createPool({
+ host: "localhost",
+ user: "root",
+ password: "root",
+ database: "exam_system",
+ });
 
-app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, '/public')))
+ app.get('/category',async (req, res) => {
+  let sql = "SELECT category_name,category_status, FORMAT(Date(created_date),'yyyy/mm/dd') AS Created_Date FROM category";
+   let [query] = await db.query(sql);
+   console.log(query);
+   res.render('category',{data : query});
+ })
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "exam_system",
-});
+  app.get('/', (req, res) => {
+    res.render("dashboard")
+ });
 
-app.get('/category',async (req, res) => {
-  let sql = "SELECT category_name,category_status,FORMAT(Date(created_date),'yyyy/mm/dd') AS Created_Date FROM category";
-  
-  let [query] = await db.query(sql);
-  console.log(query);
-  res.render('category',{data : query});
-})
-
-app.get('/', (req, res) => {
-
-  res.render("dashboard")
-});
-
-app.listen(port, () => console.log(`  port connected to ${port}!`))
+  app.listen(port, () => console.log(`  port connected to ${port}!`))
