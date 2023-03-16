e5ed6ac73bf43eb6f30f05f747093c2c85eab224
mk7102 <manoj.bajiya.2023@gmail.com>
2023-03-16 14:00:16 +0530

    Added async await in dashboard

--- kartik_index.js
+++ kartik_index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const port = 8765;
 
 app.set("view engine", "ejs");
@@ -9,13 +9,13 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, '/public')))
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "root",
   database: "exam_system",
 });
-db.connect();
+
 
 app.get('/', (req, res) => {
   res.render("dashboard")
