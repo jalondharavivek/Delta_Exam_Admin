406a65ceaa55c4d2570ef821d462298251560ef9
mk7102 <manoj.bajiya.2023@gmail.com>
2023-03-21 19:24:35 +0530

    23DEV060: Add pagination on searching

--- public/js/category.js
+++ public/js/category.js
@@ -29,7 +29,6 @@ async function check(id, status)
     {
         sts = "enable";
     }
-
     if (confirm(`Are you sure you want to ${sts}`)) {
         try {
             const result = await fetch(`http://localhost:8765/categorystatus`, {
@@ -40,14 +39,28 @@ async function check(id, status)
                 body: JSON.stringify({ id, status })
             });
             let data = await result.json();
-            window.location.reload();
+            let table = document.getElementById("myTable");
+            let html = `<tr>
+                            <th>ID</th>
+                            <th>Category Name</th>
+                            <th>Created Date</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>`;
+            data.forEach(d => {
+                html += `<tr><td>${ d.category_id }</td><td>${ d.category_name }</td><td>${ (new Date(d.created_date).toLocaleDateString()) }</td><td><a class="btnn" id="status" onclick="check(${ d.category_id },${ d.category_status });">${ d.category_status }</a></td><td><a class="edit-btn fas fa-edit" onclick="editCategory(${ d.category_id })"> EDIT</a></td></tr>`
+            })
+            table.innerHTML = html;
+            console.log(data);
+
+            // window.location.reload();
         }
         catch (err) {
             console.log(err);
         }
        
     }
-    
+     cat_status();
 }
 let dialogues = document.querySelector('.dialogues');
 dialogues.style.display = 'none';
@@ -62,6 +75,9 @@ async function addCategory()
         // if(ans)
         // {
             // console.log(ans[0].category_name);
+            
+
+            document.getElementById("overlay").style.display="block";
             let dialogues = document.querySelector('.dialogues');
             let text = document.getElementById('text');
             let id = document.getElementById('id');
@@ -74,9 +90,12 @@ async function addCategory()
             yesButton.addEventListener('click',async function() {
                 console.log("yes");
                 dialogues.style.display = 'none';
+                document.getElementById("overlay").style.display="none";
+                // document.style.display="none";
             });
             noButton.addEventListener('click', function() {
                 dialogues.style.display = 'none';
+                document.getElementById("overlay").style.display="none";
             });
             dialogues.style.display = 'block';
         // }
@@ -95,6 +114,7 @@ async function editCategory(id)
     try 
     {
         // console.log(id);
+        document.getElementById("overlay").style.display="block";
         const result= await fetch(`http://localhost:8765/editCategory?id=${id}`);
         const ans = await result.json();
         if(ans)
@@ -112,9 +132,11 @@ async function editCategory(id)
             yesButton.addEventListener('click',async function() {
                 console.log("yes");
                 dialogue.style.display = 'none';
+                document.getElementById("overlay").style.display="none";
             });
             noButton.addEventListener('click', function() {
                 dialogue.style.display = 'none';
+                document.getElementById("overlay").style.display="none";
             });
             dialogue.style.display = 'block';
         }
@@ -130,6 +152,8 @@ async function search(name)
     // console.log(name);
     let result = await fetch(`http://localhost:8765/search?name=${name}`);
     let data = await result.json();
+    let page = document.getElementById("pagination");
+    let pages = '';
     let table = document.getElementById("myTable");
     let html = `<tr>
                     <th>ID</th>
@@ -138,24 +162,33 @@ async function search(name)
                     <th>Status</th>
                     <th>Action</th>
                 </tr>`;
-    console.log(Object.keys(data.search).length);
-    if(Object.keys(data).length == 0)
-    {
-        html +=`<tr><td colspan=5>No record found</td></tr>`;
-    }
-    else
+    if(Object.keys(data.search).length != 0)
     {
         data.search.forEach(d => {
             html += `<tr><td>${ d.category_id }</td><td>${ d.category_name }</td><td>${ (new Date(d.created_date).toLocaleDateString()) }</td><td><a class="btnn" id="status" onclick="check(${ d.category_id },${ d.category_status });">${ d.category_status }</a></td><td><a class="edit-btn fas fa-edit" onclick="editCategory(${ d.category_id })"> EDIT</a></td></tr>`
         })
+
+        pages = `<li class="page-item"><a class="page-link" id="0" onclick="page(this,'${name}')">First</a></li>`;
+            for(let i=data.page;i<=Math.ceil(data.total/data.limit);i++){
+                pages +=`<li class="page-item"><a class="page-link" onclick="page(this,'${name}')" id="${i}">${i}</a></li>`
+            }
+        pages +=`<li class="page-item"><a class="page-link" onclick="page(this,'${name}')" id="${Math.ceil(data.total/data.limit)}">Last</a></li>`;
+
+    }
+    else if(Object.keys(data.search).length == 0)
+    {
+        html +=`<tr><td colspan=5>No record found</td></tr>`;
+        pages += '';
     }
     table.innerHTML = html;
+    page.innerHTML=pages;
     cat_status();
 }
 
-async function page(pages)
+async function page(pages,name = '')
 {
     let table = document.getElementById('myTable');
+    let pagination = document.getElementById('pagination');
     let html=`<tr>
                 <th>ID</th>
                 <th>Category Name</th>
@@ -163,11 +196,54 @@ async function page(pages)
                 <th>Status</th>
                 <th>Action</th>
             </tr>`;
-    var results = await fetch(`http://localhost:8765/categorypage?page=${pages.id}`);
+    let page = pages.id;
+    const results = await fetch(`http://localhost:8765/categorypage`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ page, name })
+    });
+    // var results = await fetch(`http://localhost:8765/categorypage?page=${pages.id}`);
     var data = await results.json();
     data.pages.forEach(c => {
         html += `<tr><td>${ c.category_id }</td><td>${ c.category_name }</td><td>${ (new Date(c.created_date).toLocaleDateString()) }</td><td><a class="btnn" id="status" onclick="check(${ c.category_id },${ c.category_status });">${ c.category_status }</a></td><td><a class="edit-btn fas fa-edit" onclick="editCategory(${ c.category_id })"> EDIT</a></td></tr>`;
     })
+
+    let pagi ='' ;
+    pagi += `<li class="page-item"><a class="page-link" id="0" onclick='page(this)'>First</a></li>`;
+    if(parseInt(data.page) <=5) {
+        for(let i=1;i<=parseInt(data.page);i++) {  
+            pagi += `<li class="page-item"><a class="page-link" id='${i}' onclick='page(this)'>${i}</a></li>`
+        }
+    }
+    else {
+        for(let i=(parseInt(data.page)-5);i<=parseInt(data.page);i++){
+            pagi += `<li class="page-item"><a class="page-link" id='${i}' onclick='page(this)'>${i}</a></li>`
+        }
+    }
+    if(Math.ceil((data.total)/(data.limit))-5 >= (data.page)) {
+        for(let i=parseInt(data.page)+1;i<=parseInt(data.page)+5;i++){
+            pagi += `<li class="page-item"><a class="page-link" id='${i}' onclick='page(this)'>${i}</a></li>`
+        } 
+    } 
+    else {
+        for(let i=parseInt(data.page)+1;i<=Math.ceil(parseInt(data.total)/parseInt(data.limit));i++) {
+            console.log("manoijsjdhvasd");
+            pagi += `<li class="page-item"><a class="page-link" id='${i}' onclick='page(this)'>${i}</a></li>`
+        }
+    }
+
+    pagi += `<li class="page-item"><a class="page-link" onclick='page(this)' id="${Math.ceil(data.total/data.limit)}">Last</a></li>`        
+
+    pagination.innerHTML = pagi;
+
+
+
+
     table.innerHTML = html;
+
+
+
     cat_status();
 }
--- routes/manoj_index.js
+++ routes/manoj_index.js
@@ -25,7 +25,10 @@ app.get('/category',async (req, res) => {
     let sql = `SELECT * FROM category `;
     let page = req.query.page || 1;
     let limit = req.query.limit || 10;
-  
+    if (req.query.page > 1)
+        sql += ` LIMIT ${((page - 1) * limit)}, ${limit}`;
+    else
+        sql += ` LIMIT ${limit} `;
     let [query] = await db.execute(sql);
     let sql1 = "select count(*) as total from category";
     let [result1] = await db.execute(sql1);
@@ -33,21 +36,26 @@ app.get('/category',async (req, res) => {
     res.render('category',{ data : query, page : page, total: result1[0].total, limit: limit });
 })
 
-app.get('/categorypage', async (req,res) => {
+app.post('/categorypage', async (req,res) => {
 
     let sql = `SELECT * FROM category `;
 
-    let page=parseInt(req.query.page)||1;
-    let limit=parseInt(req.query.limit)||10;
+    console.log(req.body.page);
+    let page=parseInt(req.body.page)||1;
+    let limit=parseInt(req.body.limit)||10;
     let startindex=(page-1)*limit;
     let endindex=page*limit-startindex;
-
+    if (req.body.page > 1)
+        sql += ` LIMIT ${((page - 1) * limit)}, ${limit}`;
+    else
+        sql += ` LIMIT ${limit} `;
     let [query] = await db.execute(sql);
 
     let sql1 = "select count(*) as total from category";
     let [result1] = await db.execute(sql1);
 
-    let pages = `select * from category limit ${startindex},${endindex}`;
+    let pages = `select * from category where category_name like '%${req.body.name}%' limit ${startindex},${endindex}`;
+    console.log(pages );
     let [pages1] = await db.execute(pages);
     res.json({ data : query, page: page, total: result1[0].total, limit: limit, pages : pages1 });
 })
@@ -56,17 +64,30 @@ app.post('/categorystatus',async (req, res) => {
     let id = req.body.id;
     let status = req.body.status
     // console.log(i);
+    let page=parseInt(req.query.page)||1;
+    let limit=parseInt(req.query.limit)||10;
+    let startindex=(page-1)*limit;
+    let endindex=page*limit-startindex;
+    let sql1 = `select * from category `;
+    if (req.query.page > 1)
+        sql1 += ` LIMIT ${((page - 1) * limit)}, ${limit}`;
+    else
+        sql1 += ` LIMIT ${limit} `;
     if(status == 0)
     {
         let sql = `update category set category_status = '1' where category_id = ${id}`;
         let [query] = await db.execute(sql);
-        res.json(query);
+        let [result] = await db.execute(sql1);
+        // console.log("1");
+        res.json(result);
     }
     else if(status == 1)
     {
         let sql = `update category set category_status = '0' where category_id = ${id}`;
         let [query] = await db.execute(sql);
-        res.json(query);
+        let [result] = await db.execute(sql1);
+        // console.log("0");
+        res.json(result);
     }
 })
 
@@ -96,7 +117,7 @@ app.post('/addcategory',async (req, res) => {
 
 app.get('/search',async (req, res) => {
     let sql = `SELECT * FROM category `;
-    console.log(req.query);
+    // console.log(req.query);
     let page=parseInt(req.query.page)||1;
     let limit=parseInt(req.query.limit)||10;
     let startindex=(page-1)*limit;
@@ -110,13 +131,11 @@ app.get('/search',async (req, res) => {
 
     let pages = `select * from category limit ${startindex},${endindex}`;
     let [pages1] = await db.execute(pages);
-    console.log(result1);
+    // console.log(result1);
 
     let srch = `select * from category where category_name like '%${name}%' limit ${startindex},${endindex}`;
     let [query1] = await db.query(srch);
     res.json({search : query1,data : query, page: page, total: result1[0].total, limit: limit, pages : pages1 });
-    // res.json({query});
-
 })  
 
 module.exports = app;
