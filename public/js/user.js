e910e56c5b3efe60f56f720721bc7a9cd98f5905
kartikmodi223 <kartik.modi.023@gmail.com>
2023-03-22 14:02:26 +0530

    23dev032 user module edit detail of user

--- public/js/user.js
+++ public/js/user.js
@@ -1,24 +1,24 @@
+
+function togglecolorchnage() {
+    let btn = document.querySelectorAll('.btn');
+    btn.forEach(e => {
+
+        console.log(e);
+        if (e.innerHTML == '0') {
+
+            e.innerHTML = 'DISABLE';
+            e.style.color = 'red';
+            e.style.cursor = 'pointer';
+        }
+        else if (e.innerHTML == '1') {
+
+            e.innerHTML = 'ENABLE';
+            e.style.color = 'blue';
+            e.style.cursor = 'pointer';
+        }
+    })
+}
 togglecolorchnage();
-function togglecolorchnage(){
-let btn = document.querySelectorAll('.btn');
-btn.forEach(e => {
-
-console.log(e);
-if (e.innerHTML == '0') {
-
-e.innerHTML = 'DISABLE';
-e.style.color = 'red';
-e.style.cursor = 'pointer';
-}
-else if (e.innerHTML == '1') {
-
-e.innerHTML = 'ENABLE';
-e.style.color = 'blue';
-e.style.cursor = 'pointer';
-}
-})
-}
-
 
 function toggle(status, id) {
     console.log(id)
@@ -39,5 +39,118 @@ function toggle(status, id) {
             }
         }
     }).catch(err => console.log(err));
+}
+
+
+
+async function page(pages, name = '') {
+    let table = document.getElementById('myTable');
+    let pagination = document.getElementById('pagination');
+    let html = `   <tr>
+    <th>s_id</th>
+    <th>name</th>
+    <th>email</th>
+    <th>contact</th>
+    <th>gender</th>
+    <th>address</th>
+    <th>status</th>
+    <th>state</th>
+    <th>city</th>
+    <th>college_id</th>
+    <th>created_date</th>
+    <th>action</th>
+
+</tr>`;
+    let page = pages.id;
+    const results = await fetch(`http://localhost:8765/userpage`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ page, name })
+    });
+    var student = await results.json();
+    student.pages.forEach(c => {
+        html += `   <tr>
+        <td>
+            ${c.student_id}
+        </td>
+        <td>
+        ${c.name}
+        </td>
+        <td>
+        ${c.email}
+        </td>
+        <td>
+        ${c.contact}
+        </td>
+        <td>
+        ${c.gender}
+        </td>
+        <td>
+        ${c.address}
+        </td>
+        <td>
+
+        <div id="toggle ${c.student_id}">
+            <p class="btn" id=" ${c.student_id}"
+                onclick="toggle(' ${c.student_status}','${c.student_id}')">
+                ${c.student_status}
+            </p>
+        </div>
+
+
+    </td>
+    <td>
+    ${c.state_name}
+    </td>
+    <td>
+    ${c.city}
+    </td>
+    <td>
+    ${c.college_name}
+    </td>
+    <td>
+        <%= (new Date(${c.created_date}).toLocaleDateString()) %>
+
+    </td>
+    <td>
+        <a id="editbutton" href="/edit/:id=${c.student_status}">edit
+        </a>
+    </td>
+</tr>
+`;
+    })
+
+    if (name == '') {
+        let pagi = '';
+        pagi += `<li class="page-item"><a class="page-link" id="0" onclick='page(this)'>First</a></li>`;
+        if (parseInt(student.page) <= 5) {
+            for (let i = 1; i <= parseInt(student.page); i++) {
+                pagi += `<li class="page-item"><a class="page-link" id='${i}' onclick='page(this)'>${i}</a></li>`
+            }
+        }
+        else {
+            for (let i = (parseInt(student.page) - 5); i <= parseInt(student.page); i++) {
+                pagi += `<li class="page-item"><a class="page-link" id='${i}' onclick='page(this)'>${i}</a></li>`
+            }
+        }
+        if (Math.ceil((student.total) / (student.limit)) - 5 >= (student.page)) {
+            for (let i = parseInt(student.page) + 1; i <= parseInt(student.page) + 5; i++) {
+                pagi += `<li class="page-item"><a class="page-link" id='${i}' onclick='page(this)'>${i}</a></li>`
+            }
+        }
+        else {
+            for (let i = parseInt(student.page) + 1; i <= Math.ceil(parseInt(student.total) / parseInt(student.limit)); i++) {
+                console.log("manoijsjdhvasd");
+                pagi += `<li class="page-item"><a class="page-link" id='${i}' onclick='page(this)'>${i}</a></li>`
+            }
+        }
+
+        pagi += `<li class="page-item"><a class="page-link" onclick='page(this)' id="${Math.ceil(student.total / student.limit)}">Last</a></li>`
+
+        pagination.innerHTML = pagi;
+    }
+table.innerHTML=html; 
 }
 
--- routes/user_kartik_index.js
+++ routes/user_kartik_index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-const mysql = require("mysql2");
-const port = 8765;
+const mysql = require("mysql2/promise");
+
 const bodyParser = require("body-parser");
 const { Console, log } = require('console');
 const { get } = require('http');
@@ -14,182 +14,191 @@ app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, '/public')))
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "exam_system",
-  });
-  
-  
-app.get('/user', async (req, res) => {
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "exam_system",
+});
+
+
+// app.get('/user', async (req, res) => {
+
+
+//   let sql1 = `select student_id,name,email,contact,gender,address,student_status,city,state_name,college_name 
+//   from student a, state b, colleges c where a.state_id=b.state_id and a.college_id=c.college_id`;
+
+//   [student]= await db.execute(sql1);
+//   res.render("user.ejs", { student  });
+
+// });
+
+app.get('/user',async (req, res) => {
+
+  let sql = `select student_id,name,email,contact,gender,address,student_status,city,state_name,college_name 
+  from student a, state b, colleges c where a.state_id=b.state_id and a.college_id=c.college_id`;
+
+  let page = req.query.page || 1;
+  let limit = req.query.limit || 3;
+  if (req.query.page > 1)
+      sql += ` LIMIT ${((page - 1) * limit)}, ${limit}`;
+  else
+      sql += ` LIMIT ${limit} `;
+  let [student] = await db.execute(sql);
+  let sql1 = "select count(*) as total from student";
+  let [result1] = await db.execute(sql1);
+  res.render('user',{ student , page : page, total: result1[0].total, limit: limit });
+})
+
+app.post('/userpage', async (req,res) => {
+
+  let sql = `select student_id,name,email,contact,gender,address,student_status,city,state_name,college_name 
+  from student a, state b, colleges c where a.state_id=b.state_id and a.college_id=c.college_id `;
+
+  console.log(req.body.page);
+  let page=parseInt(req.body.page)||1;
+  let limit=parseInt(req.body.limit)||3;
+  let startindex=(page-1)*limit;
+  let endindex=page*limit-startindex;
+  if (req.body.page > 1)
+      sql += ` LIMIT ${((page - 1) * limit)}, ${limit}`;
+  else
+      sql += ` LIMIT ${limit} `;
+  let [student] = await db.execute(sql);
+
+  let sql1 = "select count(*) as total from student";
+  let [result1] = await db.execute(sql1);
+
+  let pages = `select * from student where name limit ${startindex},${endindex}`;
+  console.log(pages );
+  let [pages1] = await db.execute(pages);
+  res.json({ student, page: page, total: result1[0].total, limit: limit, pages : pages1 });
+})
+
+
+
+
+
+app.get('/student_status', async (req, res) => {
+  console.log("this is call")
+  console.log(req.query)
+  let student_id = req.query.id;
+  let student_status = req.query.status;
+
+  if (student_status == '0') {
+    let status = `update student set student_status = 1 where student_id = ${student_id}`;
+    let [student_result] = await db.execute(status);
+
+    res.json({ student_result })
+
+
+  }
+  else {
+    let status = `update student set student_status = 0 where student_id = ${student_id}`;
+    let student_result = await db.execute(status);
+    res.json({ student_result })
+
+
+
+
+  }
+
+
+});
+//collage render tghrough ajax
+app.get("/collage", async (req, res) => {
+  console.log("/collage is active")
+  let id = req.query.id;
+  console.log(req.query.id);
+
+  sql1 = `select college_id from student where student_id ='${id}'`;
+  let [data1] = await db.execute(sql1);
+
+  res.send(data1);
+}
+
+)
+app.get("/allcollage", async (req, res) => {
+  console.log("allcollage")
+  let sql1 = `select * from colleges`;
+  let [data1] = await db.execute(sql1);
+  res.send(data1);
+})
+
+app.get("/edit/:id", async (req, res) => {
+
+  console.log();
+  let temp_id = req.params.id;
+  let arr = temp_id.split("=");
+
+
+  let sql3 = `select * from student where student_id='${arr[1]}';`
+  let [student2] = await db.execute(sql3);
+
+
+  let sql1 = `select * from state`;
+  let [state] = await db.execute(sql1);
+
+  res.render("edit.ejs", { student2, state });
+})
+
+
+
+app.get(`/student/allcity`, async (req, res) => {
+
+  let sql1 = `select * from city where state_id='${state_id}';`
+  let [allcity] = await db.execute(sql1);
+
+})
+app.get("/student/city", async (req, res) => {
+
+  console.log("/studebnt/city")
+  let id = req.query.state_id;
+  let sql1 = `select city_id,city_name from city where state_id='${id}'`;
+  let [data1] = await db.execute(sql1);
  
-    // console.log('enter is user')
-  
-  
-    let sql1 = `select student_id,name,email,contact,gender,address,student_status,city,state_name,college_name from student a, state b, colleges c where a.state_id=b.state_id and a.college_id=c.college_id`;
-    let student = await getdata(sql1);
-    // console.log(student)
-    res.render("user.ejs", { student});
-  
-    // console.log(student)
-  
-  });
-  
-  app.get('/student_status', async (req, res) => {
-    console.log("this is call")
-    console.log(req.query)
-    let student_id = req.query.id;
-    let student_status = req.query.status;
-  
-    if (student_status == '0') {
-      let status = `update student set student_status = 1 where student_id = ${student_id}`;
-      let student_result = await getdata(status);
-  
-      res.json({student_result})
-     
-  
-    }
-    else {
-      let status = `update student set student_status = 0 where student_id = ${student_id}`;
-      let student_result = await getdata(status);
-      res.json({student_result})
-  
-     
-     
-  
-    }
-  
-  
-  });
-  //collage render tghrough ajax
-  app.get("/collage", async (req, res) => {
-    console.log("/collage is active")
-    let id = req.query.id;
-    console.log(req.query.id);
-  
-    sql1 = `select college_id from student where student_id ='${id}'`;
-    let data1 = await getdata(sql1);
-  
-    res.send(data1);
-  }
-  
-  )
-  app.get("/allcollage", async (req, res) => {
-    console.log("allcollage")
-    let sql1 = `select * from colleges`;
-    let data1 = await getdata(sql1);
-    res.send(data1);
-  })
-  
-  app.get("/edit/:id", async (req, res) => {
-  
-    console.log();
-    let temp_id = req.params.id;
-    let arr = temp_id.split("=");
-  
-  
-    let sql3 = `select * from student where student_id='${arr[1]}';`
-    let student2 = await getdata(sql3);
-  
-  
-    //  let city_state =`select city.city_name ,state.state_name from city left join state on city.state_id = state.state_id;`
-    //  let city_state_result = await getdata(city_state);
-    //  console.log(city_state_result);
-  
-  
-    let sql1 = `select * from state`;
-    let state = await getdata(sql1);
-  
-    res.render("edit.ejs", { student2, state });
-  })
-  
-  
-  
-  app.get(`/student/allcity`, async (req, res) => {
-  
-    let sql1 = `select * from city where state_id='${state_id}';`
-    let allcity = await getdata(sql1);
-  
-  })
-  app.get("/student/city", async (req, res) => {
-  
-    console.log("/studebnt/city")
-    let id = req.query.state_id;
-    let sql1 = `select city_id,city_name from city where state_id='${id}'`;
-    let data1 = await getdata(sql1);
-    // console.log(data1)
-    res.send(data1);
-  
-  });
-  
-  
-  app.get("/city", async (req, res) => {
-    console.log("/city is active")
-    let id = req.query.id;
-    // console.log(req.query.id);
-  
-    sql1 = `select city from student where student_id ='${id}'`;
-    let data1 = await getdata(sql1);
-    // console.log(data1);
-    res.send(data1);
-  })
-  
-  app.post("/update", async (req, res) => {
-  
-  
-    // console.log(req.body);
-    let student_id = req.body.student_id;
-    let name = req.body.name;
-    let email = req.body.email;
-    let contact = req.body.contact;
-    let gender = req.body.gender;
-    let address = req.body.address;
-    let city = req.body.city;
-    let state_id = req.body.state;
-    let college_id = req.body.col;
-  
-  
-  
-    let sql4 = `update student set name='${name}',email='${email}',contact='${contact}',gender='${gender}',
+  res.send(data1);
+
+});
+
+
+app.get("/city", async (req, res) => {
+  console.log("/city is active")
+  let id = req.query.id;
+ 
+
+  sql1 = `select city from student where student_id ='${id}'`;
+  let [data1] = await db.execute(sql1);
+ 
+  res.send(data1);
+})
+
+app.post("/update", async (req, res) => {
+
+
+
+  let student_id = req.body.student_id;
+  let name = req.body.name;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let gender = req.body.gender;
+  let address = req.body.address;
+  let city = req.body.city;
+  let state_id = req.body.state;
+  let college_id = req.body.col;
+
+
+
+  let sql4 = `update student set name='${name}',email='${email}',contact='${contact}',gender='${gender}',
      address='${address}',city='${city}', state_id = '${state_id}' ,college_id='${college_id}' where student_id=${student_id};`
-  
-    // console.log(sql4);
-  
-    let student3 = await getdata(sql4);
-    // console.log(student3);
-    res.redirect('/user')
-  
-  
-  });
-  
-  app.get('/userpage', async (req,res) => {
-  
-    let sql = `select student_id,name,email,contact,gender,address,student_status,city,state_name,college_name from student a, state b, colleges c where a.state_id=b.state_id and a.college_id=c.college_id `;
-  
-    let page=parseInt(req.query.page)||1;
-    let limit=parseInt(req.query.limit)||10;
-    let startindex=(page-1)*limit;
-    let endindex=page*limit-startindex;
-  
-    let [query] = await getdata(sql);
-  
-    let sql1 = "select count(*) as total from student a, state b, colleges c where a.state_id=b.state_id and a.college_id=c.college_id";
-    let [result1] = await getdata(sql1);
-  
-    let pages = `select student_id,name,email,contact,gender,address,student_status,city,state_name,college_name from student a, state b, colleges c where a.state_id=b.state_id and a.college_id=c.college_id limit ${startindex},${endindex}`;
-    let [pages1] = await getdata(pages);
-    
-    res.json({ data : query, page: page, total: result1[0].total, limit: limit, pages : pages1 });
-  })
-  
-  
-  
-  async function getdata(sql) {
-    return new Promise((res, rej) => {
-      db.query(sql, (err, data) => {
-        if (err) throw err;
-        res(data);
-      })
-    })
-  }
-  module.exports = app;
+  let [student3] = await db.execute(sql4);
+  res.redirect('/user')
+
+
+});
+
+
+
+
+
+
+module.exports = app;
