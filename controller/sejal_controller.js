17822380768b3361513d6beecc1a4db12096c99c
sky192002 <sejalyadavespark2023@gmail.com>
2023-03-24 15:44:40 +0530

    23DEV051  chnages in view login &  change password

--- controller/sejal_controller.js
+++ controller/sejal_controller.js
@@ -14,15 +14,15 @@ app.use(express.static(path.join(__dirname, '/public/')))
 
 
 const resultget = async(req,res) =>{
-    let sql = `select ee.exam_id , ee.exam_name ,ee.total_questions ,ee.exam_time ,ee.exam_status  from  exam_system.exam ee inner join 
-    exam_system.user_answers uans on ee.exam_id = uans.exam_id;`
+    let sql = `select ee.exam_id , ee.exam_name ,ee.total_questions ,ee.exam_time ,ee.exam_status  from  exam ee inner join 
+    user_answers uans on ee.exam_id = uans.exam_id;`
      let [query] = await db.query(sql);
      console.log(query);
      res.render('result',{data : query});
 }
 
 const viewresultget =  async(req,res) =>{
-    let sql = `SELECT category_name FROM exam_system.exam  a, exam_system.user_answers b 
+    let sql = `SELECT category_name FROM exam  a, user_answers b 
     where a.exam_id=b.exam_id`;
      let [query] = await db.query(sql);
      console.log(query);
@@ -33,14 +33,14 @@ const viewresultget =  async(req,res) =>{
 
  
 const viewquestionget = async (req,res) =>{
-    let sql = `select q.question_text , q.answer ,uans.user_answers ,  uans.marks from exam_system.questions  q inner join 
-    exam_system.user_answers uans on q.question_id=uans.question_id;`;
+    let sql = `select q.question_text , q.answer ,uans.user_answers ,  uans.marks from questions  q inner join 
+    user_answers uans on q.question_id=uans.question_id;`;
      let [query] = await db.query(sql);
      console.log(query);
 
 
-        let sql1 = `select  s.name from exam_system.student s inner join 
-        exam_system.result r on s.student_id=r.student_id`;
+        let sql1 = `select  s.name from student s inner join 
+        result r on s.student_id=r.student_id`;
         let [stu_name] = await db.execute(sql1);
         console.log(stu_name);
 
