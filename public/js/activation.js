9118f1cb62fdb684d4aa4e93ad10c201446f2f17
mk7102 <manoj.bajiya.2023@gmail.com>
2023-03-27 17:35:19 +0530

    23DEV060 : Changes in search pagination

--- public/js/activation.js
+++ public/js/activation.js
@@ -1,20 +1,7 @@
-async function activclick() {
-    var email = '<%= email %>';
-    var resultRandom = "<%= resultRandom %>";
-    console.log(email);
-    fetch(`/active/${resultRandom}`, {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email,
-                resultRandom
-            })
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.UpdateStatus);
-
-        })
+async function logout()
+{
+    if(confirm("Are you sure you want to Logout"))
+    {
+        window.location.href="/logout";
+    }
 }
