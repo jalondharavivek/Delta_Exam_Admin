d7f6d99236b5dac0a46023a438405151489b44e4
milanchudasma <milan.chudasma.23@gmail.com>
2023-03-29 14:52:08 +0530

    23dev045

--- controller/result_controller.js
+++ controller/result_controller.js
@@ -1,7 +1,7 @@
 var db = require('../connection/mysql');
 require('../connection/module');
 
-let limit = 1;
+let limit = 2;
 const studentlist = async (req, res) => {
 
   var data = [];
@@ -23,14 +23,14 @@ const studentlist = async (req, res) => {
   }
 
 
-  var sql1 = `select distinct a.name,b.user_id,exam_id from student a, user_answers b, user_login c where b.user_id=c.user_id and c.email = a.email limit ${offset},${limit};`
+  var sql1 = `select distinct a.name,b.user_id from student a, user_answers b, user_login c where b.user_id=c.user_id and c.email = a.email limit ${offset},${limit};`
   let [query1] = await db.query(sql1);
 
 
   var sql2 = `select distinct a.name,b.user_id from student a, user_answers b, user_login c where b.user_id=c.user_id and c.email = a.email;`;
   let [query2] = await db.query(sql2);
 
-  let count = query2.length / limit;
+  let count = Math.ceil(query2.length / limit);
 
   res.render('result', { data: query1, count, limit, curpage });
 
@@ -64,7 +64,7 @@ const studentlistpage = async (req, res) => {
   var sql2 = `select distinct a.name,b.user_id from student a, user_answers b, user_login c where b.user_id=c.user_id and c.email = a.email;`;
   let [query2] = await db.query(sql2);
 
-  let count = query2.length / limit;
+  let count = Math.ceil(query2.length / limit);
 
 
   res.json({ data: query1, count, limit, curpage });
@@ -105,7 +105,7 @@ const companylist = async (req, res) => {
   let [query2] = await db.query(sql2);
 
 
-  let count = query2.length / limit;
+  let count = Math.ceil(query2.length / limit);
 
 
 
@@ -145,7 +145,7 @@ const companylistpage = async (req, res) => {
   let [query2] = await db.query(sql2);
 
 
-  let count = query2.length / limit;
+  let count = Math.ceil(query2.length / limit);
 
 
 
@@ -180,7 +180,7 @@ const getexamdetaile = async (req, res) => {
   let [query2] = await db.query(sql2);
 
 
-  let count = query2.length / limit;
+  let count = Math.ceil(query2.length / limit);
 
   res.render('getexamdetaile', { data: query, count, limit, curpage, id });
 }
@@ -212,7 +212,7 @@ const getexamdetailepage = async (req, res) => {
   let [query2] = await db.query(sql2);
 
 
-  let count = query2.length / limit;
+  let count = Math.ceil(query2.length / limit);
 
 
 
@@ -244,7 +244,7 @@ const viewquestionget = async (req, res) => {
   var sql2 = `select distinct question_text,answer,user_answers from questions a, user_answers b where a.question_id=b.question_id and category_id=${id}`;
   let [query2] = await db.query(sql2);
 
-  let counter = query2.length / limit;
+  let counter = Math.ceil(query2.length / limit) ;
 
 
   res.render('viewquestionresult', { data: query, counter, limit, curpage, id });
@@ -275,7 +275,7 @@ const viewquestiongetpage = async (req, res) => {
   var sql2 = `select distinct question_text,answer,user_answers from questions a, user_answers b where a.question_id=b.question_id and category_id=${id}`;
   let [query2] = await db.query(sql2);
 
-  let counter = query2.length / limit;
+  let counter = Math.ceil(query2.length / limit) ;
 
   res.json({ data: query, counter, limit, curpage, id });
 }
