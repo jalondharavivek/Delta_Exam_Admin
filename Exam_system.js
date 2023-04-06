9f9e83bf52b65a46537dc8532960fc1a463c7e68
mk7102 <manoj.bajiya.2023@gmail.com>
2023-04-06 12:34:45 +0530

    Error solved

--- Exam_system.js
+++ Exam_system.js
@@ -1,40 +1,26 @@
 const express = require('express');
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
-var db = require('./src/connection/mysql');
 const sessions = require('express-session');
-var cookie = require('cookie-parser');
-var utils = require('util');
-const { decode } = require('punycode');
 let bodyParser = require('body-parser')
-const flash = require('connect-flash');
-var nodemailer = require('nodemailer');
 const path = require('path')
 const app = express();
+require("dotenv").config( './.env' );
 app.use(express.static('public'));
 app.use(sessions({
-    secret: "huy7uy7u",
+    secret: process.env.SECRET_KEY,
     saveUninitialized: true,
     resave: false,
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, 
       },
   }));
-const ejs = require('ejs');
-const { signedCookie } = require('cookie-parser');
-const { Console } = require('console');
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/assets/image'));
 app.use(express.static(__dirname + ''));
 const PORT = process.env.PORT;
 app.set('view engine', 'ejs');
 
-app.use(cookie());
-app.use(flash());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 const router = require('./src/routes/route')
